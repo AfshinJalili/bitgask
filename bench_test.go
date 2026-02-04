201f5f0a17a4d61d50b1e39317241f89ccd4042e
Afshin Jalili <afshnjalili@gmail.com>
2026-02-04 05:17:15 +0330

    bench: expand iteration benchmarks

--- bench_test.go
+++ bench_test.go
@@ -1,30 +1,111 @@
 package bitgask
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 	"testing"
 	"time"
 )
 
-func benchOptions() Options {
-	return Options{
-		SyncOnPut:            false,
-		SyncOnDelete:         false,
-		SyncInterval:         time.Hour,
-		MergeInterval:        time.Hour,
-		MergeMinTotal:        1 << 30,
-		MergeTriggerRatio:    0.9,
-		Compression:          CompressionNone,
-		UseHintFiles:         true,
-		HintFileSync:         false,
-		MaxDataFileSize:      1 << 20,
-		CompressionThreshold: 256,
+const (
+	benchIterKeyCount  = 10000
+	benchIterValueSize = 128
+	benchIterKeyCountLarge  = 100000
+	benchIterValueSizeLarge = 1024
+	benchIterValueSizeXL    = 4096
+)
+
+var (
+	benchCountSink int
+)
+
+func benchPopulate(b *testing.B, count int, valueSize int) *DB {
+	opts := benchOptions()
+	opts = append(opts, WithMaxDataFileSize(1<<30))
+	db, err := Open(b.TempDir(), opts...)
+	if err != nil {
+		b.Fatalf("open: %v", err)
+	}
+	value := bytes.Repeat([]byte("a"), valueSize)
+	for i := 0; i < count; i++ {
+		key := []byte(fmt.Sprintf("key-%06d", i))
+		if err := db.Put(key, value); err != nil {
+			_ = db.Close()
+			b.Fatalf("put: %v", err)
+		}
+	}
+	return db
+}
+
+func validateIterKeys(b *testing.B, db *DB, expected int) {
+	count := 0
+	if err := db.IterKeys(context.Background(), func(_ []byte) bool {
+		count++
+		return true
+	}); err != nil {
+		b.Fatalf("iterkeys: %v", err)
+	}
+	if count != expected {
+		b.Fatalf("iterkeys count: %d", count)
+	}
+}
+
+func validateIter(b *testing.B, db *DB, expected int) {
+	count := 0
+	if err := db.Iter(context.Background(), nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+		count++
+		return true
+	}); err != nil {
+		b.Fatalf("iter: %v", err)
+	}
+	if count != expected {
+		b.Fatalf("iter count: %d", count)
+	}
+}
+
+func validateKeys(b *testing.B, db *DB, expected int) {
+	count := 0
+	for range db.Keys() {
+		count++
+	}
+	if count != expected {
+		b.Fatalf("keys count: %d", count)
+	}
+}
+
+func validateScan(b *testing.B, db *DB, expected int) {
+	count := 0
+	if err := db.Scan(nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+		count++
+		return true
+	}); err != nil {
+		b.Fatalf("scan: %v", err)
+	}
+	if count != expected {
+		b.Fatalf("scan count: %d", count)
+	}
+}
+
+func benchOptions() []Option {
+	return []Option{
+		WithSyncOnPut(false),
+		WithSyncOnDelete(false),
+		WithSyncInterval(time.Hour),
+		WithMergeInterval(time.Hour),
+		WithMergeMinTotal(1 << 30),
+		WithMergeTriggerRatio(0.9),
+		WithCompression(CompressionNone),
+		WithHintFiles(true),
+		WithHintFileSync(false),
+		WithMaxDataFileSize(1 << 20),
+		WithCompressionThreshold(256),
 	}
 }
 
 func BenchmarkPut(b *testing.B) {
 	opts := benchOptions()
-	db, err := Open(b.TempDir(), opts)
+	db, err := Open(b.TempDir(), opts...)
 	if err != nil {
 		b.Fatalf("open: %v", err)
 	}
@@ -42,7 +123,7 @@ func BenchmarkPut(b *testing.B) {
 
 func BenchmarkGet(b *testing.B) {
 	opts := benchOptions()
-	db, err := Open(b.TempDir(), opts)
+	db, err := Open(b.TempDir(), opts...)
 	if err != nil {
 		b.Fatalf("open: %v", err)
 	}
@@ -67,9 +148,8 @@ func BenchmarkGet(b *testing.B) {
 
 func BenchmarkMerge(b *testing.B) {
 	opts := benchOptions()
-	opts.MergeMinTotal = 0
-	opts.MergeTriggerRatio = 0
-	db, err := Open(b.TempDir(), opts)
+	opts = append(opts, WithMergeMinTotal(0), WithMergeTriggerRatio(0))
+	db, err := Open(b.TempDir(), opts...)
 	if err != nil {
 		b.Fatalf("open: %v", err)
 	}
@@ -91,5 +171,237 @@ func BenchmarkMerge(b *testing.B) {
 		if err := db.Merge(MergeOptions{Force: true}); err != nil {
 			b.Fatalf("merge: %v", err)
 		}
+	}
+}
+
+func BenchmarkIterKeys(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCount, benchIterValueSize)
+	defer db.Close()
+
+	validateIterKeys(b, db, benchIterKeyCount)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.IterKeys(context.Background(), func(_ []byte) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("iterkeys: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkKeys(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCount, benchIterValueSize)
+	defer db.Close()
+
+	validateKeys(b, db, benchIterKeyCount)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		for key := range db.Keys() {
+			_ = key
+			count++
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkIter(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCount, benchIterValueSize)
+	defer db.Close()
+
+	validateIter(b, db, benchIterKeyCount)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.Iter(context.Background(), nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("iter: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkScan(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCount, benchIterValueSize)
+	defer db.Close()
+
+	validateScan(b, db, benchIterKeyCount)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.Scan(nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("scan: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkIterKeysLarge(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeLarge)
+	defer db.Close()
+
+	validateIterKeys(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.IterKeys(context.Background(), func(_ []byte) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("iterkeys: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkKeysLarge(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeLarge)
+	defer db.Close()
+
+	validateKeys(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		for range db.Keys() {
+			count++
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkIterLarge(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeLarge)
+	defer db.Close()
+
+	validateIter(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.Iter(context.Background(), nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("iter: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkScanLarge(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeLarge)
+	defer db.Close()
+
+	validateScan(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.Scan(nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("scan: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkIterKeysXL(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeXL)
+	defer db.Close()
+
+	validateIterKeys(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.IterKeys(context.Background(), func(_ []byte) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("iterkeys: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkKeysXL(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeXL)
+	defer db.Close()
+
+	validateKeys(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		for range db.Keys() {
+			count++
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkIterXL(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeXL)
+	defer db.Close()
+
+	validateIter(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.Iter(context.Background(), nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("iter: %v", err)
+		}
+		benchCountSink = count
+	}
+}
+
+func BenchmarkScanXL(b *testing.B) {
+	db := benchPopulate(b, benchIterKeyCountLarge, benchIterValueSizeXL)
+	defer db.Close()
+
+	validateScan(b, db, benchIterKeyCountLarge)
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		count := 0
+		if err := db.Scan(nil, func(_ []byte, _ []byte, _ RecordMeta) bool {
+			count++
+			return true
+		}); err != nil {
+			b.Fatalf("scan: %v", err)
+		}
+		benchCountSink = count
 	}
 }
